8a50aa438f11f9b929ac0631ebd20ec3be859fdb
tahir ghori <tahirghoori@gmail.com>
2020-01-13 02:41:22 +0500

    first app

--- src/main/java/com/ms/base/BaseApplication.java
+++ src/main/java/com/ms/base/BaseApplication.java
@@ -1,13 +1,28 @@
 package com.ms.base;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
 
 @SpringBootApplication
 public class BaseApplication {
 
+	@Value("${db.driverClassName}")
+	private String driverClass;
+	@Value("${db.url}")
+	private String url;
+	@Value("${db.username}")
+	private String username;
+	@Value("${db.password}")
+	private String password;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BaseApplication.class, args);
 	}
-
 }
